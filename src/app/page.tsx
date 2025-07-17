b10ac751e8924c4457bd98b413e145cd77ba3aa0
Anjan Bag <abag694@gmail.com>
2025-07-17 12:45:24 +0530

    03 : trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Client } from "./client";
 
-
-const Home = async () => {
-  
+ 
+const Home = () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text : "Anjan Prefetch" }));
   return (
-    <div className="text-rose-500">
-      
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
